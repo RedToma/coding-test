b12af33aac783c8fd5f7bc6218076e63771bdbd5
RedToma <101503429+RedToma@users.noreply.github.com>
2024-10-13 18:06:27 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.31 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,27 +1,19 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = new int[4];
-        int lux = 51;
-        int luy = 51;
-        int rdx = 0;
-        int rdy = 0;
+        int[] answer = {51, 51, 0, 0};
 
         for (int i = 0; i < wallpaper.length; i++) {
             for (int j = 0; j < wallpaper[i].length(); j++) {
-                if(wallpaper[i].charAt(j) == '#') {
-                    lux = Math.min(lux, i);
-                    luy = Math.min(luy, j);
-                    rdx = Math.max(rdx, i + 1);
-                    rdy = Math.max(rdy, j + 1);
+                char c = wallpaper[i].charAt(j);
+
+                if (c == '#') {
+                    answer[0] = Math.min(answer[0], i);
+                    answer[1] = Math.min(answer[1], j);
+                    answer[2] = Math.max(answer[2], i + 1);
+                    answer[3] = Math.max(answer[3], j + 1);
                 }
             }
         }
-        
-        answer[0] = lux;
-        answer[1] = luy;
-        answer[2] = rdx;
-        answer[3] = rdy;
-        
         
         return answer;
     }
