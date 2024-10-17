b241c135e8cc7f5bed0c16d997812b8dd674902a
RedToma <101503429+RedToma@users.noreply.github.com>
2024-10-17 21:29:17 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.85 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        
+        return answer -= Arrays.stream(numbers).sum();
+    }
+}
