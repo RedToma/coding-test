0bf0099fafb651ed2841a2b4073fd3efde6dbc51
RedToma <101503429+RedToma@users.noreply.github.com>
2024-10-17 21:28:20 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 49.62 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,35 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int f_len = friends.length;
+        int[][] exchange = new int[friends.length][friends.length];
+        int[] point = new int[friends.length];
         Map<String, Integer> idx = new HashMap<>();
-        int[] point = new int[f_len];
-        int[][] exchange = new int[f_len][f_len];
         int answer = 0;
 
-        for (int i = 0; i < f_len; i++) idx.put(friends[i], i);
+        for (int i = 0; i < friends.length; i++) idx.put(friends[i], i);
 
         for (String gift : gifts) {
             String[] split = gift.split(" ");
-            String from = split[0];
-            String to = split[1];
 
-            point[idx.get(from)]++;
-            point[idx.get(to)]--;
-            exchange[idx.get(from)][idx.get(to)]++;
+            point[idx.get(split[0])]++;
+            point[idx.get(split[1])]--;
+            exchange[idx.get(split[0])][idx.get(split[1])]++;
         }
 
-        for (int i = 0; i < f_len; i++) {
+        for (int i = 0; i < friends.length; i++) {
             int gift = 0;
-            for (int j = 0; j < f_len; j++) {
+            for (int j = 0; j < friends.length; j++) {
                 if (friends[i].equals(friends[j])) continue;
 
-                if (exchange[i][j] > exchange[j][i] ||
-                        (exchange[i][j] == exchange[j][i] && point[i] > point[j])) {
+                if (exchange[i][j] > exchange[j][i] || (exchange[i][j] == exchange[j][i] && point[i] > point[j])) {
                     gift++;
                 }
             }
-            if (gift > answer) answer = gift;
+            answer = Math.max(answer, gift);
         }
         
         return answer;
